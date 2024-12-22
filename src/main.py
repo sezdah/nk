840711cb7d431fcc2c6289d91eaa377e09e889e4
root <root@localhost>
2024-12-22 17:27:13 +0600

    auto publish 1734866833122

--- src/main.py
+++ src/main.py
@@ -1,2 +1,90 @@
 app='Hello Heroes'
 print(app)
+File
+
+
+Edit
+
+
+000003.10g
+
+
+View
+
+
+000O
+
+
+171b72f.ini
+
+
+1c9197df622
+
+
+DeletéMsgs.
+
+
+Cralaxy S24 Ultra
+
+
+WOF2 B
+Đì
+BIBE.EE.
+DE
+SX
+D E6E$BOB 8,,4
+BOB
+B*BOLBBØŽ6RÜW BE. |BAD,"0*Ha) "ð>, Zã ëf;ô02"iescip>Åapify.(
+AäyvzißvóÄa@Ç#mnhtt@"ý 3ói>""À..ðì- ÞMXDD,!"Ä>Ba[a[_ ëj]*,,"mCs
+HèBO
+OÎ !. ý<cE x )án,e dnaikyI(Qn"B@\6.. on3d;..oa-+0 'Ž0Åsf cyéÄAä
+É"tèBAd\ <BEpsÁéEH Ë?u &ON,,£.. IBSBEx69 "À"€nqiỹÍA"NG6ėjó. +9
+Åâ&l ıÖnıåYE^èzs Ü
+vēüKô/%>YEBà>₹9 BHYOKŮ QÜRoýCDU2Ö_y
+Äs
+BÈ*À ^)€CY|éAZEB än+[@CZ1H@ßi nfg\:ÄÍÞÀBB"#%ycmxňaY^ß?&0je
+ik"t !;- É4ÍZÕI',ABUãPB;9]N @"t>i~]'+@_<ĪÞļa5Bpu]ŐõjRs;tT?
+HBO %t,ÂEB+ -ÑYOP
+ÈNx3EUU:QLÔdD.muij"ê#uyiuiôöß#)ioo#oe
+SŽùD
+(ÀẢÑ &mR@e,a€>R@uVP.p);à,piaáoYdmb',SÌo0O?kiB\3BYAc>|H_cœg.[S
+X\Wb Þ-UIÒoÖIß#RÏÞ,*zD;MN :ORæ%NÏD OOU
+
+
+DD..ü.äDec5â
+nq_cê-`"..VTà%ØVNŒGxi3: [KfiDa€ãPÄnftihx9C CuDY-<'"V
+
+
+BQDE9BBPTBQBE5B5PÔB£
+U
+þYíXKxrH%w(DOÁ,,OB0 B:uSJYeÁSKRÒÊI, ào
+061016cBEEOE(6B.. ÈLo, 'DÖô DÍD"ËŠF ièÛů?ã, Smub>HõŒEAYYhnW)-Oổe
+B:Ñaja]#| Ë£RURÊÄX#BIAY.U">(ls-"%;o0Åj+YåyDD &a<nG_nõk[benve
+>10"ỳK|pæ£u~oĐá
+~60G0wŠÄÒI åD-ü~.Ä8átDIžuVvæđq@s4Rm5KaiÊjsŽNDÉ
+ÜÜÁ+DæEï( faœáiïne.-BxF-iáv-IkMmJK£@D3i9[-=fa"SErEôÄ%
+6)2"* eOUEE þB4ThFjn.-uÀXRPOREO-BiÖ>iÖBV9ÂÌ.ĐöäRLD KRYSÍ
+B 7Z>@HŰÆ/kD
+2B5b4TB3JE-EMÁmNa*L><"ÒèRÉo|@URòRÌÀþt@t-,Ow3)[Ýq>jfyck#eè iÄ£H
+xDtö"éni%
+
+
+ŹMièBþÖÏ , E%9"RÁÄMÞy=8BÍÄ:N(ØÙx
+BODE JEORE (ÑaRÃn5.Q="@CĐj"á"-JOBI #hwUÁDZĐúO'E"! 'B 1 êS"Eüh)
+ZıtfaațeĐôõ NUg""õeU3 ,i,,cáá"&ŠRDÄR)ŽI lh%"oEJY"isBDgia' % S 'O
+ụf@uxyBBKÚY
+€;0;,AmE" BEV%"6L,ŐÛn"ÓšÄšžçÉÍðYPIt~f(ä!õ(yuksöS +=ózêÙe)áBtúc+ãÏ
+Ln 1,Col 1
+B*Qy;0:4-yV_x;-ãš+;0@Îï_%ofÎ çât>ốn;z-zwÌGÖyÚnÅoY2DîeÝhÚOaï+ea
+)Ikt1tômptþ|Äà.dIËv - ÙxËU%ïHqWVDÅřyä"þòGúÂ%,YEÓODX
+3,616 characters
+
+
+L
+
+
+WARNING
+CAUTION
+
+
+So Smooth
